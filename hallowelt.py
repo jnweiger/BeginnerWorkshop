faa925b347bd57b7bad6f14af79c4a4b991565a9
alphaFred <alphaFred@users.noreply.github.com>
2017-01-05 19:53:04 +0100

    modified hallowelt.py to support 2.x and 3.x

--- hallowelt.py
+++ hallowelt.py
@@ -1,22 +1,22 @@
-import tkinter 
-
-from tkinter.constants import *
-
+try:
+    import tkinter
+except ImportError:
+    import Tkinter as tkinter
 
 tk = tkinter.Tk()
 
 
-frame = tkinter.Frame(tk, relief = RIDGE, borderwidth = 2)
-frame.pack(fill = BOTH, expand = 1)
+frame = tkinter.Frame(tk, relief=tkinter.RIDGE, borderwidth=2)
+frame.pack(fill=tkinter.BOTH, expand=1)
 
 
-label = tkinter.Label(frame, text = "Hallo, Welt!")
-label.pack(fill = X, expand = 1)
+label = tkinter.Label(frame, text="Hallo, Welt!")
+label.pack(fill=tkinter.X, expand=1)
 
 
-button = tkinter.Button(frame, text = "Exit", 
-          command = tk.destroy)
-button.pack(side = BOTTOM, padx = 4, pady = 4)
+button = tkinter.Button(frame, text="Exit",
+          command=tk.destroy)
+button.pack(side=tkinter.BOTTOM, padx=4, pady=4)
 
 
 tkinter.mainloop()
